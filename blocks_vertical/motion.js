727681b02a2443afd0d5c332c0d7d239738d659b
peiYM <peiyumo@163.com>
2019-11-12 09:46:34 +0800

    1.调整event语句块定义，包括变量名、选项等 2.build.py调整为windows版本 3.导入play.png

--- blocks_vertical/motion.js
+++ blocks_vertical/motion.js
@@ -693,13 +693,14 @@ Blockly.Blocks['motion_motorBall_rotate_on_speed_for_seconds'] = {
           ]
         },
         {
-          // "type": "input_value",
-          // "name": "speed",
-          "type": "field_speedBellDialog",
+          "type": "input_value",
           "name": "speed",
-          "value": "0",
-          "min": "0",
-          "max": "255"
+
+          // "type": "field_speedBellDialog",
+          // "name": "speed",
+          // "value": "0",
+          // "min": "0",
+          // "max": "255"
         },
         {
           "type": "input_value",
@@ -731,13 +732,14 @@ Blockly.Blocks['motion_motorBall_rotate_on_speed'] = {
           ]
         },
         {
-          // "type": "input_value",
-          // "name": "speed",
-          "type": "field_speedBellDialog",
+          "type": "input_value",
           "name": "speed",
-          "value": "0",
-          "min": "0",
-          "max": "255"
+
+          // "type": "field_speedBellDialog",
+          // "name": "speed",
+          // "value": "0",
+          // "min": "0",
+          // "max": "255"
         }
       ],
       "category": Blockly.Categories.motion,
